71a5551d406b78ba569e74e1bf674eaf7be117f4
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-02 15:45:11 +0300

    curves added

--- SpringAnimationApp/Services/DataStorage.swift
+++ SpringAnimationApp/Services/DataStorage.swift
@@ -5,4 +5,40 @@
 //  Created by Vsevolod Lashin on 02.05.2023.
 //
 
-
+final class DataStorage {
+    let shared = DataStorage()
+    
+    let curves = [
+        "easeIn",
+        "easeOut",
+        "easeInOut",
+        "linear",
+        "spring",
+        "easeInSine",
+        "easeOutSine",
+        "easeInOutSine",
+        "easeInQuad",
+        "easeOutQuad",
+        "easeInOutQuad",
+        "easeInCubic",
+        "easeOutCubic",
+        "easeInOutCubic",
+        "easeInQuart",
+        "easeOutQuart",
+        "easeInOutQuart",
+        "easeInQuint",
+        "easeOutQuint",
+        "easeInOutQuint",
+        "easeInExpo",
+        "easeOutExpo",
+        "easeInOutExpo",
+        "easeInCirc",
+        "easeOutCirc",
+        "easeInOutCirc",
+        "easeInBack",
+        "easeOutBack",
+        "easeInOutBack"
+    ]
+    
+    private init() {}
+}
